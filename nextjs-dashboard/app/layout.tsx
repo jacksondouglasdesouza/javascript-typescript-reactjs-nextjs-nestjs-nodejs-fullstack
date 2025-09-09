c683fdcefb724db977297d78f7020b345cb94490
jacksondouglasdesouza <jacksondouglasdesouza@hotmail.com>
2025-09-08 22:33:38 -0300

    Class NextJS

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,17 @@
+import React, { Children } from 'react';
+
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode;
+}){
+  return(
+    <html lang='en'>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
